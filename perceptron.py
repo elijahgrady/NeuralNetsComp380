b56b144cd9008c46876cc49b3794f9976b065b4c
Setareh Lotfi <setarehlotfi@Setarehs-MacBook-Air.local>
2017-02-27 17:44:44 -0800

    init training data class

--- perceptron.py
+++ perceptron.py
@@ -42,6 +42,7 @@ Draw your conclusion if there is one
 '''
 
 
+vector = []
 
 def prompt():
     print('Welcome to our first neural network - A Perceptron Net!\n')
@@ -109,33 +110,34 @@ class Net:
 def readFile(s):
     global inputDimension
     global outputDimension
-    global numberTraining
-
-    vector = []
+    global numberOfTraining
+    global vector
+    global stringVector
     output = " "
     f = open(s,'r')
     f.readline()
     f.readline()
     inputDimension = [int(s) for s in f.readline().split() if s.isdigit()]
     outputDimension = [int(s) for s in f.readline().split() if s.isdigit()]
-    numberTraining =  [int(s) for s in f.readline().split() if s.isdigit()]
-    print("imput %d, output %d, number training %d",inputDimension,outputDimension,numberTraining)
+    numberOfTraining =  [int(s) for s in f.readline().split() if s.isdigit()]
+    print("imput %d, output %d, number training %d",inputDimension,outputDimension,numberOfTraining)
     f.readline()
     m = f.readline()
     x =len(m.strip())
-    stri = ""
 
     kk = [True for i in m if i.isalpha()]
     while (x !=0  and kk != True):
-        vector.append(m.strip("\n").replace(" ", ""))
-        stri.join(m.strip("\n").replace(" ", ""))
+        vector.append(m.strip("\n"))
         m = f.readline()
         print(m.strip())
         x = len(m.strip().replace(" ", ""))
         kk = [True for i in m if i.isalpha()]
 
+    stringVector= (' '.join(x.strip() for x in vector if x.strip()))
 
-    print("vector is %s\n" % vector)
+
+    print("vector is %s\n" % stringVector)
+
     output = f.readline().strip("\n").replace(" ", "")
     print(output)
 
@@ -143,16 +145,31 @@ def readFile(s):
 
 
 def main():
-    global training_data_file_name
+    global weight
     prompt()
     while (1):
         training_data = input(
             'Enter 1 to train using a training data file, enter 2 to train using a trained weights file : ')
         if training_data == '1':
             training_data_file_name = input('Enter the training data file name : ')
+            #Parse stuff for the file
             readFile(training_data_file_name)
+
             training_data_weights = input(
                 'Enter 0 to initialize weight to 0, or, enter 1 to initialize weights to random values between -0.5 and 0.5 : ')
+            if (training_data_weights == 1):
+                weight = random.uniform(-0.5, 0.5)
+            else:
+                weight = 0
+            #Steup the training data set
+            for i in range(0,numberOfTraining[0]):
+                for x in stringVector.split():
+                    TrainingData(int(x),weight,numberOfTraining[0])
+
+
+
+
+
             training_data_max_epochs = input('Enter the maximum number of training epochs : ')
             training_data_output_weights = input('Enter a file name to save the trained weight settings : ')
             training_data_alpha_rate = input('Enter the learning rate alpha from >0 to 1 : ')
